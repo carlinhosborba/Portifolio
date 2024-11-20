4884b3b474f618a4db24f1fd1f9ff3502c449f0a
carlinhosborba <161654338+carlinhosborba@users.noreply.github.com>
2024-11-20 03:04:12 +0000

    ok

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
-    if (document.getElementById("submitGuess")) {
-      const secretCode = generateCode();
-      let attempts = [];
-  
-      document.getElementById("submitGuess").addEventListener("click", () => {
-        const guess = document.getElementById("guess").value;
-        if (!/^\d{4}$/.test(guess)) {
-          alert("Digite um código válido com 4 dígitos.");
-          return;
-        }
-        const result = checkGuess(guess, secretCode);
-        attempts.unshift(`Palpite: ${guess} - Resultado: ${result}`);
-        updateAttempts();
-        document.getElementById("guess").value = "";
-      });
-  
-      document.getElementById("revealCode").addEventListener("click", () => {
-        alert(`O código secreto é: ${secretCode}`);
-      });
-  
-      function generateCode() {
-        return String(Math.floor(1000 + Math.random() * 9000));
-      }
-  
-      function checkGuess(guess, code) {
-        let bulls = 0;
-        let cows = 0;
-        for (let i = 0; i < 4; i++) {
-          if (guess[i] === code[i]) {
-            bulls++;
-          } else if (code.includes(guess[i])) {
-            cows++;
-          }
-        }
-        return `${bulls} Bulls, ${cows} Cows`;
-      }
-  
-      function updateAttempts() {
-        const attemptsList = document.getElementById("attempts");
-        attemptsList.innerHTML = "";
-        attempts.forEach((attempt) => {
-          const li = document.createElement("li");
-          li.textContent = attempt;
-          attemptsList.appendChild(li);
-        });
-      }
-    }
+let progress = 0;
+const progressBar = document.querySelector(".progress");
+const loadingContainer = document.getElementById("loading-container");
+const mainContent = document.getElementById("main-content");
+
+function updateProgress() {
+  if (progress < 100) {
+    progress += 3;
+    progressBar.style.width = progress + "%";
+    setTimeout(updateProgress, 50);
+  } else {
+    loadingContainer.style.transition = "opacity 1s ease";
+    loadingContainer.style.opacity = "0";
+    setTimeout(() => {
+      loadingContainer.style.display = "none";
+      mainContent.style.display = "block";
+      mainContent.style.opacity = "1";
+    }, 500);
+  }
+}
+
+updateProgress();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-links li a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navLinks.forEach((link) =>
+        link.parentElement.classList.remove("selected")
+      );
+      this.parentElement.classList.add("selected");
+    });
   });
-  
+
+  const toggleThemeButton = document.getElementById("toggle-theme");
+  toggleThemeButton.addEventListener("click", function () {
+    document.body.classList.toggle("light-mode");
+  });
+
+  document
+    .querySelector('a[href="#contato"]')
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      let targetSection = document.querySelector("#contato");
+
+      window.scrollTo({
+        top: targetSection.getBoundingClientRect().top + window.scrollY - 100,
+        behavior: "smooth",
+      });
+    });
+
+  const fadeIns = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  fadeIns.forEach((fadeIn) => {
+    observer.observe(fadeIn);
+  });
+});
